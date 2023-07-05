c0092083059dd09ed4a072cb5be6074ce6f01082
Fabricio felix <126669160+FabricioFelix-25@users.noreply.github.com>
2023-07-05 16:19:01 -0300

    Add files via upload

---
+++ FelixBanco/FelixBanco/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace FelixBanco
+{
+    public class ContaCorrente
+
+    {
+        private int Agencia;
+        private int Numero;
+
+        public static double TaxaOperacao { get; private set; }
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            agencia = agencia;
+            numero = numero;
+
+            try 
+            { 
+
+                TotalDeContasCriadas++;
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+
+                Console.WriteLine("0 resultado é: " + TaxaOperacao);
+
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("nao podemos dividir por 0");
+                
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(9999,123456);
+            ContaCorrente conta2 = new ContaCorrente(8888, 223456);
+            ContaCorrente conta3 = new ContaCorrente(1111, 323456);
+
+
+            Console.ReadLine();
+        }
+    }
+}
